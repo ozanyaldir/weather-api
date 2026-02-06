eb0ff20af7aaa1849d49912b63e7a6d6c62c7afc
Ozan Yaldır <ozanyaldir@gmail.com>
2026-02-06 18:07:12 +0300

    Initialized database connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"log"
+	"weather-api/internal/database"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables")
+	}
+
+	if err := database.Connect(); err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer database.Close()
 }
---
+++ internal/database/connection.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func Connect() error {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var err error
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		return fmt.Errorf("error opening database: %w", err)
+	}
+
+	DB.SetMaxOpenConns(25)
+	DB.SetMaxIdleConns(5)
+	DB.SetConnMaxLifetime(5 * time.Minute)
+
+	if err = DB.Ping(); err != nil {
+		return fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	log.Println("Database connected successfully")
+	return nil
+}
+
+func Close() error {
+	if DB != nil {
+		return DB.Close()
+	}
+	return nil
+}
